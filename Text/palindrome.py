af74fc3cb836555a3ee39a9e8eda7c9254befd50
JohannesKepler <kwatzkwatz@gmail.com>
2014-01-13 16:32:35 -0500

    Update palindrome.py
    Hey, I'm new to programming and I was looking at your code to help myself learn. And I thought this bit could be improved with a couple extra lines to ignore punctuation and spaces! Now palindromes like "Madam, I'm adam," will work!

--- Text/palindrome.py
+++ Text/palindrome.py
@@ -4,9 +4,11 @@ by the user is a palindrome. That is that it reads
 the same forwards as backwards like "racecar"
 """
 
-string = raw_input('Enter a string: ').lower()
+string = raw_input('Enter a string: ')
+new_string = ''.join(e for e in string if e.isalnum())
+new_string = new_string.lower()
 
-if string == string[::-1]:
+if new_string == new_string[::-1]:
     print '%s is a palindrome' % string
 else:
     print '%s is not a palindrome' % string
